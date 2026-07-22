45ed933d7932240faad60dac55b13935cf817546
Taku-market <lucky.mill0418@gmail.com>
2026-07-23 07:16:22 +0900

    Add logging to tests

--- test_all_users.py
+++ test_all_users.py
@@ -1,7 +1,13 @@
+from logger import get_logger
+
+logger = get_logger(__name__)
+
 def test_all_users_have_email(all_users):
     for user in all_users:
+        logger.info(f"Checking email for: {user['name']}")
         assert "@" in user["email"]
 
 def test_all_users_have_name(all_users):
     for user in all_users:
+        logger.info(f"Checking name for: {user['name']}")
         assert len(user["name"]) > 0
